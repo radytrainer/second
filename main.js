0ed2974b61f0e3e8440199accc306496cd591ef9
radytrainer <rady.y@passerellesnumeriques.org>
2021-05-06 15:57:07 +0700

    background

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ const chalk = require('chalk');
 const url = "https://random-words-api.vercel.app/word";
 axios
 .get(url)
-.then( response => console.log(chalk.bgRed(response.data[0].word)));
+.then( response => console.log(chalk.bgGreen(response.data[0].word)));
