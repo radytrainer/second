ed0ab6fed1fd658f76d6d6f3e57d9af71f825e71
rady y <rady.y@passerellesnumeriques.org>
2021-05-06 15:50:38 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const axios = require('axios');
+const chalk = require('chalk');
+
+const url = "https://random-words-api.vercel.app/word";
+axios
+.get(url)
+.then( response => console.log(chalk.bgRed(response.data[0].word)));
